c307a274845452cc002a93ba664d373e7bd049a9
helpclearclear <jmemberr72@gmail.com>
2021-05-09 16:06:52 -0400

    Update PythonCheats.py

--- PythonCheats.py
+++ PythonCheats.py
@@ -3,3 +3,9 @@
 def unixTimeStamp(stamp):
     import requests as R
     return R.get(f'https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp={stamp}').text
+
+#When you want to retrieve an mp4 file via url, use this function. Make sure the string value of 'saveToFileName' is the name/path of a local file ending in '.mp4':
+
+def retrieveMP4ByURL(url, saveToFileName):
+    import urllib.request as perform
+    return perform.urlretrieve(url, saveToFileName)
